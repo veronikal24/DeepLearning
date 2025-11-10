05924453a5f7d2ef8fcce920fd1a462e4779f505
KevinFlummi <s242823@dtu.dk>
2025-11-10 14:56:24 +0100

    Changed job for tptrans

--- job.sh
+++ job.sh
@@ -5,7 +5,7 @@
 #BSUB -e error_%J.err                 # Error log file
 #BSUB -q c02516                          # Queue name (gpu queue)
 #BSUB -gpu "num=1:mode=exclusive_process"   # Request 1 GPU
-#BSUB -R "rusage[mem=10G]"             # Request 8 GB of RAM
+#BSUB -R "rusage[mem=10G]"             # Request 10 GB of RAM
 #BSUB -W 01:00                        # Time limit (1 hour)
 #BSUB -R "span[hosts=1]"             # Number of CPUs
 #BSUB -n 4                            # Number of CPU cores
@@ -17,11 +17,11 @@
 # Load the cuda module to make it accessible to pytorch
 module load cuda
 
-# change directory into your home (i think you have to change the part on your machine (to find your home path, just type 'echo $HOME'))
+# changes directory into your home (change this to your own home path (which you can find with 'echo $HOME'))
 cd /zhome/b1/8/213657/
 # sources the python virtual environment (not sure if you need this, probably not if you installed everything in the base python)
 source venv/bin/activate
-# change directory into the deep learning project folder
+# changes directory into the deep learning project folder
 cd DeepLearning
-# execute the script
-python main.py
+# executes the script
+python tptrans.py
