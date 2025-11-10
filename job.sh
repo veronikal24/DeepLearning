d4ccf75723e9c83ad22287ad59b9d24180fbe05d
KevinFlummi <s242823@dtu.dk>
2025-11-10 15:40:55 +0100

    Changed GPU cluster

--- job.sh
+++ job.sh
@@ -3,19 +3,18 @@
 #BSUB -J DL_Boats                  # Job name
 #BSUB -o output_%J.log                # Output log file (including job ID %J)
 #BSUB -e error_%J.err                 # Error log file
-#BSUB -q c02516                          # Queue name (gpu queue)
+#BSUB -q gpuv100                          # Queue name (gpu queue)
 #BSUB -gpu "num=1:mode=exclusive_process"   # Request 1 GPU
 #BSUB -R "rusage[mem=10G]"             # Request 10 GB of RAM
 #BSUB -W 01:00                        # Time limit (1 hour)
 #BSUB -R "span[hosts=1]"             # Number of CPUs
 #BSUB -n 4                            # Number of CPU cores
 
-# When we want to execute the script on GPU, we need to run this job in the terminal with:
-# ./job.sh
-# It uses some scheduling system which uses these bsub arguments above to assign ressources somehow :)
-# Maybe the terminal will tell you "Permission denied" when running ./job.sh, in that case you need to run:
+# Run this command first to make the script executable:
 # chmod +x job.sh
-# before and try to run the job again.
+# Then, when wanting to run on GPU, we need to run this job in the terminal with:
+# bsub < ./job.sh
+# It uses some scheduling system which uses these bsub arguments above to assign ressources somehow :)
 
 # Load the cuda module to make it accessible to pytorch
 module load cuda
