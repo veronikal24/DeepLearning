18ed484e5ba41e458c3b76533765ed7d1d4e4f23
KevinFlummi <s242823@dtu.dk>
2025-11-16 16:17:05 +0100

    Use argparse for hyperparameters

--- command_scripts/job.sh
+++ command_scripts/job.sh
@@ -3,10 +3,10 @@
 #BSUB -J DL_Boats                  # Job name
 #BSUB -o logs/output_%J.log                # Output log file (including job ID %J)
 #BSUB -e logs/error_%J.err                 # Error log file
-#BSUB -q gpua10                          # Queue name (gpu queue)
+#BSUB -q gpuv100                          # Queue name (gpu queue)
 #BSUB -gpu "num=1:mode=exclusive_process"   # Request 1 GPU
-#BSUB -R "rusage[mem=2G]"             # Request 5 GB of RAM
-#BSUB -W 02:00                        # Time limit (2 hours)
+#BSUB -R "rusage[mem=5G]"             # Request 5 GB of RAM
+#BSUB -W 05:00                        # Time limit (5 hours)
 #BSUB -R "span[hosts=1]"             # Number of CPUs
 #BSUB -n 4                            # Number of CPU cores
 
@@ -20,11 +20,12 @@
 module load cuda
 
 # changes directory into your home (change this to your own home path (which you can find with 'echo $HOME'))
-#cd /zhome/b1/8/213657/
-cd /zhome/63/7/219953/
+cd /zhome/b1/8/213657/
+#cd /zhome/63/7/219953/
+
 # sources the python virtual environment (not sure if you need this, probably not if you installed everything in the base python)
 source venv/bin/activate
 # changes directory into the deep learning project folder
-cd DeepLearning-1
+cd DeepLearning
 # executes the script
-python tptrans.py
+python tptrans.py --k 2000 --epochs 500 --ds_window_total 600 --ds_window_pred 240 
