d50596d4de6f8bb9981e653767bd8a9f62707efe
KevinFlummi <s242823@dtu.dk>
2025-11-19 10:02:12 +0100

    Job updates

--- command_scripts/temptpi.sh
+++ command_scripts/temptpi.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#BSUB -J TPInform                  # Job name
-#BSUB -o logs/output_tpi_%J.log                # Output log file (including job ID %J)
-#BSUB -e logs/error_tpi_%J.err                 # Error log file
+#BSUB -J TempTPI                 # Job name
+#BSUB -o logs/output_temptpi_%J.log                # Output log file (including job ID %J)
+#BSUB -e logs/error_temptpi_%J.err                 # Error log file
 #BSUB -q gpuv100                          # Queue name (gpu queue)
 #BSUB -gpu "num=1:mode=exclusive_process"   # Request 1 GPU
-#BSUB -R "rusage[mem=4G]"             # Request 5 GB of RAM
+#BSUB -R "rusage[mem=4G]"             # Request 4 GB of RAM
 #BSUB -W 06:00                        # Time limit (5 hours)
 #BSUB -R "span[hosts=1]"             # Number of CPUs
 #BSUB -n 4                            # Number of CPU cores
@@ -22,10 +22,9 @@ module load cuda
 # changes directory into your home (change this to your own home path (which you can find with 'echo $HOME'))
 cd /zhome/b1/8/213657/
 #cd /zhome/63/7/219953/
-
 # sources the python virtual environment (not sure if you need this, probably not if you installed everything in the base python)
 source venv/bin/activate
 # changes directory into the deep learning project folder
 cd DeepLearning
 # executes the script
-python tpinform.py --k 1000 --epochs 300 --ds_window_total 420 --ds_window_pred 120
+python temptpi.py --k 1000 --epochs 300 --ds_window_total 420 --ds_window_pred 120
